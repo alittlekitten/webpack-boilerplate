84b8c4848c3b4f0db8d0ba0da7e7470a1e58b3c3
alittlekitten <dlscjs8646@gmail.com>
2021-09-09 05:13:43 +0900

    [FEAT] 이미지 추가, js파일 개선, dist 위치 변경
    사용자가 받을 수 있는 파일을 public에 깔끔하게 정리하기 위해서
    dist 위치를 public 안으로 이동했습니다.
    또한 사진 파일을 추가하고 js파일을 index.js 하나로 압축했습니다.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,2 +1,22 @@
-import img from '../images/puppy.jpg'
-console.log("test");
+import puppy from '../images/puppy.jpg';
+import kitten from '../images/kitten.jpg';
+import '../sass/index.scss';
+
+
+const renderMainpage = () => {
+    const component = document.querySelector("body");
+    console.log(component);
+    component.innerHTML = `
+        <h1>익스프레스</h1>
+        <p>Welcome to Express</p>
+        <img src="${puppy}">
+        <img src=${kitten}>
+        <script src="/../../dist/bundle.js"></script>
+    `
+}
+
+const init = () => {
+    renderMainpage(); 
+}
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: ['./public/javascripts/index.js', './public/sass/index.scss'],
   // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/dist'),
     filename: 'bundle.js'
   },
   module: {
@@ -40,9 +40,10 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
-                    MiniCssExtractPlugin.loader, // style-loader와 MiniCssExtractPlugin.loader를 함께 사용하면 안된다!
+                    "style-loader", // style-loader와 MiniCssExtractPlugin.loader를 함께 사용하면 안된다!
                     "css-loader",   // translates CSS into CommonJS
-                    "sass-loader"   // compiles Sass to CSS, using Node Sass by default
+                    "sass-loader",   // compiles Sass to CSS, using Node Sass by default
+                    //  MiniCssExtractPlugin.loader : 별도의 css로 빼낸다 - 효율적이지만, css파일을 요청하기때문에 페이지 랜딩 속도가 느릴 것으로 예상
                 ],
                 exclude: /node_modules/
             },
@@ -64,10 +65,10 @@ module.exports = {
             },
     ]
   },
-  plugins: [
+  plugins: [ 
     // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
     new MiniCssExtractPlugin({
-        filename: '/css/style.css'
+        filename: 'public/dist/css/style.css'
     }),
     new HtmlWebpackPlugin({
         template: 'public/index.html'
